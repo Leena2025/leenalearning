b29ffd0aabc20991fc81190d6ad8d2df02ec6273
Leena2025 <leenaasokan09@gmail.com>
2025-03-08 19:48:48 +0530

    day 1

---
+++ Day 1/day1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class middleelement {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        int size=scan.nextInt();
+        int[] num=new int[size];
+
+        for(int i=0;i<=size-1;i=i+1){
+           num[i]=scan.nextInt(); 
+        }
+         
+
+
+        
+
+    }
+
+    
+}
